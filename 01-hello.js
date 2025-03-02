803ece7388fc2abf7a270172d5fa88629b4125ae
aabagdi <70984836+aabagdi@users.noreply.github.com>
2025-03-02 13:36:44 -0800

    first commit

---
+++ 01-hello.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", loadPage);
+
+function loadPage(event) {
+  console.log("The page has now loaded.");
+  console.log(
+    "I'm a CS grad student, and I have two terms left. My main passion when it comes to CS is iOS development, but outside of CS I love films and music! I am excited to learn about the web during this class!"
+  );
+}
